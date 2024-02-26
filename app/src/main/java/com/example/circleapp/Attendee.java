5297fe9be12245ab004f9416c27f0863b8082ecb
hanmaynas <hannah.mayanja@gmail.com>
2024-02-26 16:08:31 -0700

    adding stuff in attendee class

--- app/src/main/java/com/example/circleapp/Attendee.java
+++ app/src/main/java/com/example/circleapp/Attendee.java
@@ -1,4 +1,39 @@
 package com.example.circleapp;
 
+import java.lang.reflect.Array;
+
 public class Attendee {
+    private String name;
+    private String email;
+    // private profilePic
+    private Array events;
+
+    public Attendee(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Array getEvents() {
+        return events;
+    }
+
+    public void setEvents(Array events) {
+        this.events = events;
+    }
 }
