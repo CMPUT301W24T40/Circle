4babaa4dc56ce3a2cf176cd0754cb5497eaf0d8d
Rachit Anugula <anugula@ualberta.ca>
2024-02-28 10:50:24 -0700

    Created fragments for profile, yourEvents, and browseEvents + respective layout files; edited MainActivity and activity_main.xml to change up the implementation of the nav bar a bit; renamed menu.xml file and events_icon png files; still need to work on logic to switch between fragments

--- app/src/main/java/com/example/circleapp/MainActivity.java
+++ app/src/main/java/com/example/circleapp/MainActivity.java
@@ -1,26 +1,48 @@
 package com.example.circleapp;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.Menu;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.circleapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new BrowseEventsFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+
+                case 1000000: // number represents the ID number of your_events_item
+                    replaceFragment(new YourEventsFragment());
+                    break;
+                case 1000004: // number represents the ID number of browse_events_item
+                    replaceFragment(new BrowseEventsFragment());
+                    break;
+                case 1000003: // number represents the ID number of profile_item
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
