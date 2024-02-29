86ed542b3c9136649a1021fca3d7b96de11f4e53
hanmaynas <hannah.mayanja@gmail.com>
2024-02-29 14:26:09 -0700

    added attendee info to profile fragment class

--- app/src/main/java/com/example/circleapp/ProfileFragment.java
+++ app/src/main/java/com/example/circleapp/ProfileFragment.java
@@ -4,14 +4,41 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
+import java.util.Objects;
+
 public class ProfileFragment extends Fragment {
+
+    EditText firstNameEditText;
+    EditText lastNameEditText;
+    EditText emailEditText;
+    EditText phoneNumberEditText;
+    CheckBox geolocationEditText;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+//        firstNameEditText = (EditText) getView().findViewById(R.id.first_name);
+//        lastNameEditText = (EditText) getView().findViewById(R.id.last_name);
+//        emailEditText = (EditText) getView().findViewById(R.id.email);
+//        phoneNumberEditText = (EditText) getView().findViewById(R.id.phone_number);
+//        geolocationEditText = (CheckBox) getView().findViewById(R.id.geolocation);
+////
+//        String firstName = firstNameEditText.getText().toString();
+//        String lastName = lastNameEditText.getText().toString();
+//        String name = firstName + lastName;
+//        String email = emailEditText.getText().toString();
+//        Integer phoneNumber = Integer.valueOf(phoneNumberEditText.getText().toString());
+//        boolean isGeoEnabled = geolocationEditText.isEnabled();
+//
+//        Attendee user = new Attendee(name, email);
+//        user.setGeoEnabled(isGeoEnabled);
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
