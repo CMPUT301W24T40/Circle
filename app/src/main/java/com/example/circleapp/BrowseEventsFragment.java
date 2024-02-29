9ff1b0bb011e3cf32354b19c46c4eefba1a76390
Rachit Anugula <anugula@ualberta.ca>
2024-02-29 09:36:08 -0700

    Just converted the "dummy data implementation" to a for loop to improve readability

--- app/src/main/java/com/example/circleapp/BrowseEventsFragment.java
+++ app/src/main/java/com/example/circleapp/BrowseEventsFragment.java
@@ -34,21 +34,9 @@ public class BrowseEventsFragment extends Fragment {
     private List<Event> getEvents() {
         // Use dummy data for now
         List<Event> events = new ArrayList<>();
-        events.add(new Event("Event 1", "Location 1", "Date 1", "Time 1", "Description 1"));
-        events.add(new Event("Event 2", "Location 2", "Date 2", "Time 2", "Description 2"));
-        events.add(new Event("Event 3", "Location 3", "Date 3", "Time 3", "Description 3"));
-        events.add(new Event("Event 4", "Location 4", "Date 4", "Time 4", "Description 4"));
-        events.add(new Event("Event 5", "Location 5", "Date 5", "Time 5", "Description 5"));
-        events.add(new Event("Event 6", "Location 6", "Date 6", "Time 6", "Description 6"));
-        events.add(new Event("Event 7", "Location 7", "Date 7", "Time 7", "Description 7"));
-        events.add(new Event("Event 8", "Location 8", "Date 8", "Time 8", "Description 8"));
-        events.add(new Event("Event 9", "Location 9", "Date 9", "Time 9", "Description 9"));
-        events.add(new Event("Event 10", "Location 10", "Date 10", "Time 10", "Description 10"));
-        events.add(new Event("Event 11", "Location 11", "Date 11", "Time 11", "Description 11"));
-        events.add(new Event("Event 12", "Location 12", "Date 12", "Time 12", "Description 12"));
-        events.add(new Event("Event 13", "Location 13", "Date 13", "Time 13", "Description 13"));
-        events.add(new Event("Event 14", "Location 14", "Date 14", "Time 14", "Description 14"));
-        events.add(new Event("Event 15", "Location 15", "Date 15", "Time 15", "Description 15"));
+        for (int i = 1; i <= 15; i++) {
+            events.add(new Event("Event " + i, "Location " + i, "Date " + i, "Time " + i, "Description " + i));
+        }
         return events;
     }
 }
