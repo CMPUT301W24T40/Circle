1404937645bdf75aa4fb695328c8f664c225d683
Syrus Muhammed <syrus.muham@gmail.com>
2024-03-08 13:37:38 -0700

    Added EventUnitTest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,9 +47,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation ("androidx.test.espresso:espresso-contrib:3.5.1")
-    androidTestImplementation ("androidx.test.espresso:espresso-intents:3.5.1")
-    androidTestImplementation ("androidx.fragment:fragment-testing:1.6.2")
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.7.3"))
     implementation("com.google.firebase:firebase-analytics")
