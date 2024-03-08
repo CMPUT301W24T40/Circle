65be770b1ff5ce63a8a575494c53e8db9bbb32eb
Syrus Muhammed <syrus.muham@gmail.com>
2024-03-08 13:44:39 -0700

    Added EventUnitTest

--- app/src/test/java/com/example/circleapp/EventUnitTest.java
+++ app/src/test/java/com/example/circleapp/EventUnitTest.java
@@ -15,20 +15,20 @@ public class EventUnitTest {
 
         // Set values using setters
         event.setID("123");
-        event.setEventName("Sample Event");
-        event.setLocation("Sample Location");
+        event.setEventName("Event1");
+        event.setLocation("Location1");
         event.setDate("2024-03-01");
         event.setTime("10:00 AM");
-        event.setDescription("Sample Description");
+        event.setDescription("Description1");
         event.setEventPoster(12345);
 
         // Check using getters
         assertEquals("123", event.getID());
-        assertEquals("Sample Event", event.getEventName());
-        assertEquals("Sample Location", event.getLocation());
+        assertEquals("Event1", event.getEventName());
+        assertEquals("Location1", event.getLocation());
         assertEquals("2024-03-01", event.getDate());
         assertEquals("10:00 AM", event.getTime());
-        assertEquals("Sample Description", event.getDescription());
+        assertEquals("Description1", event.getDescription());
         assertEquals(12345, event.getEventPoster());
     }
 
