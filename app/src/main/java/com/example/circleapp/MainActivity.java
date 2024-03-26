39350901780ec5c8b63bfdb346c24edab66d19b2
Rachit Anugula <anugula@ualberta.ca>
2024-03-25 19:18:38 -0600

    Added Admin capabilities -> users can now become admin through password entry and are able to browse events and profiles for now, not images; no deletion capabilities implemented yet; only bug is that user isnt immediately switched to admin interface, only switched after app restart

--- app/src/main/java/com/example/circleapp/MainActivity.java
+++ app/src/main/java/com/example/circleapp/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.circleapp;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.example.circleapp.Admin.AdminHomeFragment;
 import com.example.circleapp.EventDisplay.BrowseEventsFragment;
 import com.example.circleapp.EventDisplay.YourEventsFragment;
 import com.example.circleapp.Profile.ProfileFragment;
@@ -24,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param savedInstanceState If the activity is being re-initialized after previously being
      *                           shut down then this Bundle contains the data it most recently
-     *                           supplied in onSaveInstanceState(Bundle)
+     *                           supplied in savedInstanceState(Bundle)
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,27 +35,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         firebaseManager.setPhoneID(this);
-        replaceFragment(new BrowseEventsFragment()); // Start with BrowseEventsFragment
 
-        // Bottom navigation item selection listener
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-            // Replace fragment based on the selected item
-            if (item.getItemId() == R.id.your_events_item) {
-                replaceFragment(new YourEventsFragment());
-                return true;
+        firebaseManager.isAdmin(exists -> {
+            if (exists) {
+                replaceFragment(new AdminHomeFragment());
             }
+            else {
+                replaceFragment(new BrowseEventsFragment()); // Start with BrowseEventsFragment
 
-            if (item.getItemId() == R.id.browse_events_item) {
-                replaceFragment(new BrowseEventsFragment());
-                return true;
+                binding.bottomNavigationView.setVisibility(View.VISIBLE);
+                // Bottom navigation item selection listener
+                binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+                    // Replace fragment based on the selected item
+                    if (item.getItemId() == R.id.your_events_item) {
+                        replaceFragment(new YourEventsFragment());
+                        return true;
+                    }
+
+                    if (item.getItemId() == R.id.browse_events_item) {
+                        replaceFragment(new BrowseEventsFragment());
+                        return true;
+                    }
+
+                    if (item.getItemId() == R.id.profile_item) {
+                        replaceFragment(new ProfileFragment());
+                        return true;
+                    }
+
+                    return false;
+                });
             }
-
-            if (item.getItemId() == R.id.profile_item) {
-                replaceFragment(new ProfileFragment());
-                return true;
-            }
-
-            return false;
         });
     }
 
