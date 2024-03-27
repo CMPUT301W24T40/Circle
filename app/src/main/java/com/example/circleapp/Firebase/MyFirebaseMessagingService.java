3b6a21786f789900f17c587f9d58dd33ebde253a
Hannah Zamora <hzamora@ualberta.ca>
2024-03-26 21:26:15 -0600

    in-app notifications should be working, banner present, but no banner yet for notifs when app is in background

--- app/src/main/java/com/example/circleapp/Firebase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/circleapp/Firebase/MyFirebaseMessagingService.java
@@ -1,11 +1,22 @@
 package com.example.circleapp.Firebase;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 
+import com.example.circleapp.MainActivity;
+import com.example.circleapp.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Objects;
 
 /**
  * This class is used for Firebase Messaging ordeals.
@@ -32,6 +43,26 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      */
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        String body = Objects.requireNonNull(remoteMessage.getNotification()).getBody();
+        String title = remoteMessage.getNotification().getTitle();
+        Intent intent = new Intent(MyFirebaseMessagingService.this, MainActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
+        String channelId = "Default";
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.ic_notification_icon)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true).setContentIntent(pendingIntent)
+                .setPriority(Notification.PRIORITY_MAX);
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            NotificationChannel channel = new NotificationChannel(channelId, "Default channel", NotificationManager.IMPORTANCE_HIGH);
+            channel.setShowBadge(true);
+            channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+            manager.createNotificationChannel(channel);
+        }
+        manager.notify(0, builder.build());
     }
 
 }
