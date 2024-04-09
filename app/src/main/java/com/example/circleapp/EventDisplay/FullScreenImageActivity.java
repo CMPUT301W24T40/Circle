62fa3d62b18f60f7a9adb764e05aa2fd0211dfd5
Takaraf <108546895+Takaraf@users.noreply.github.com>
2024-04-09 12:49:22 -0600

    Javadoc for FullScreenImageActivity

--- app/src/main/java/com/example/circleapp/EventDisplay/FullScreenImageActivity.java
+++ app/src/main/java/com/example/circleapp/EventDisplay/FullScreenImageActivity.java
@@ -13,8 +13,20 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import com.bumptech.glide.Glide;
 import com.example.circleapp.R;
 
+/**
+ * This class is used to display an image in full screen mode. The image is passed to this activity
+ * via an intent from another activity. The image is displayed with animations.
+ */
 public class FullScreenImageActivity extends AppCompatActivity {
-
+    /**
+     * Called when the activity is starting. This method is used to set the content view to the
+     * full screen image layout and display the image with a zoom-in animation and the background
+     * layout with a fade-in animation.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after previously being shut
+     *                           down then this Bundle contains the data it most recently supplied
+     *                           in onSaveInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +45,11 @@ public class FullScreenImageActivity extends AppCompatActivity {
         backgroundLayout.startAnimation(fadeInAnimation);
 
         getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            /**
+             * Called when the back button is pressed. This method is used to animate the image with
+             * a zoom-out animation and the background layout with a fade-out animation before finishing
+             * the activity.
+             */
             @Override
             public void handleOnBackPressed() {
                 Animation zoomOutAnimation = AnimationUtils.loadAnimation(FullScreenImageActivity.this, R.anim.zoom_out);
